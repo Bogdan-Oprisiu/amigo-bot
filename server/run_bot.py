5e8b0d945eae74e21936c3350800f4f96cb7072c
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2024-04-09 08:26:20 +0300

    Final touch

--- server/run_bot.py
+++ server/run_bot.py
@@ -2,7 +2,10 @@ import os
 import openai
 from dotenv import load_dotenv
 
+from fastapi import FastAPI, HTTPException
 from amigo_response import get_chat_response
+
+app = FastAPI()
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,33 +15,25 @@ api_key = os.getenv('OPEN_AI_API_KEY')
 
 # Check if the API key is empty or not found
 if not api_key:
-    print("Error: API key not found in api_key.txt or the file is empty.")
+    print("Error: API key not found in environment variables.")
+    exit()  # Exit the script if API key is not found
+
+# Instantiate the OpenAI client
+client = openai.OpenAI(api_key=api_key)
 
 
-def main():
-    if not api_key:
-        print("Error: OPENAI_API_KEY not found in environment variables.")
-        return
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-    # Instantiate the OpenAI client
-    client = openai.OpenAI(api_key=api_key)
 
-    print(
-        "Welcome to the Amigo bot! I'm your friendly and cheerful companion."
-        " Let's chat about anything! Type 'Goodbye' to end the conversation.")
-
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == 'goodbye':
-            print("Goodbye! May the force be with you!")
-            break
-
-        else:
-            response = get_chat_response(client, user_input)
-
-            if response:
-                print("Amigo: " + response)
+@app.post("/send_message/")
+async def send_message(input_string: str):
+    response = get_chat_response(client, input_string)
+    return {"response": response}
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=5001)
