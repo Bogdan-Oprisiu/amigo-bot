cee79af889a42a84a180ccd47a9ef50c970604a5
LucaTheSorcerer <lucka.tudor@gmail.com>
2024-04-09 10:40:20 +0300

    added pydantic model

--- server/run_bot.py
+++ server/run_bot.py
@@ -3,12 +3,18 @@ import openai
 from dotenv import load_dotenv
 
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
 from amigo_response import get_chat_response
 
 app = FastAPI()
 
 # Load environment variables from .env file
 load_dotenv()
+
+
+class Message(BaseModel):
+    input_string: str
 
 # Access the API key
 api_key = os.getenv('OPEN_AI_API_KEY')
@@ -28,12 +34,17 @@ async def root():
 
 
 @app.post("/send_message/")
-async def send_message(input_string: str):
-    response = get_chat_response(client, input_string)
-    return {"response": response}
+async def send_message(message: Message):
+    # response = get_chat_response(client, input_string)
+    # return {"response": response}
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": "Your AI friend"}, {"role": "user", "content": message.input_string}]
+    )
+    return {"response": response.choices[0].message.content}
 
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=5001)
+# if __name__ == "__main__":
+#     import uvicorn
+#
+#     uvicorn.run(app, host="127.0.0.1", port=5001)
